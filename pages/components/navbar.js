e4fcb15c112f03cbaade8a0bf2a68a4f435e51ab
vicsnet <adesanmiadebisi@gmail.com>
2023-09-08 23:23:10 +0100

    wallet button

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import ConnectWallet from '../components/ConnectWallet'
 
 const NavBar = () => {
   return (
@@ -12,7 +14,8 @@ const NavBar = () => {
                     height={32}
                 />
             </Link>
-            <button className="connect-wallet text-white px-4 py-2 rounded-md">Connect Wallet</button>
+            {/* <button className="connect-wallet text-white px-4 py-2 rounded-md">Connect Wallet</button> */}
+<ConnectWallet/>
         </div>
     </nav>
   );
