82c05ca8b677a3ae250678d3a57609c45a61d5b5
vicsnet <adesanmiadebisi@gmail.com>
2023-09-09 13:29:06 +0100

    navbar

--- pages/hospital/dashboard/public/index.js
+++ pages/hospital/dashboard/public/index.js
@@ -1,11 +1,4 @@
-import HospitalNavBar from '../../../components/hospital-navbar'
-export default function HospitalProfile() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <HospitalNavBar />
-    </main>
-  )
-}
+
 import HospitalNavBar from '../../../components/hospital-navbar'
 import HospitalBio from '../../../components/hospital-bio'
 export default function HospitalProfile() {
