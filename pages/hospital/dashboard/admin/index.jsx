d8bddb1465d3f200d409090fdc85211ba905e819
vicsnet <adesanmiadebisi@gmail.com>
2023-09-09 13:06:54 +0100

    merged perfectly

--- pages/hospital/dashboard/admin/index.jsx
+++ pages/hospital/dashboard/admin/index.jsx
@@ -4,7 +4,6 @@ import HospitalForm from '../../hospital-form/HospitalForm'
 import { useAccount, useContractRead } from 'wagmi'
 import donorABI from '../../../../constant/ABI/Donosynk.json'
 import {donorSynkAddress} from '../../../../constant/contract'
-<<<<<<< HEAD
 import { watch } from 'fs'
 
 export default function Admin() {
@@ -22,21 +21,6 @@ export default function Admin() {
     <div>
         {
             data ===true?
-=======
-
-export default function Admin() {
-    const {address} = useAccount()
-    const { data, isError, isLoading } = useContractRead({
-        address: donorSynkAddress,
-        abi: donorABI,
-        functionName: 'registeredHospitalStatus',
-        args:[address]
-      })
-  return (
-    <div>
-        {
-            data ==='true'?
->>>>>>> 7c4ad13 (dashboard set)
             <HospitalDashboard/>
             :
         <HospitalForm/>
