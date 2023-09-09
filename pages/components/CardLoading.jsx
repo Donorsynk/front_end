bffba91e45b1e559951a6401f229ec7a24f1d2c0
vicsnet <adesanmiadebisi@gmail.com>
2023-09-09 03:41:23 +0100

    donor-form integrated

---
+++ pages/components/CardLoading.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+export default function CardLoading() {
+  return (
+    <div className="flex mt-4">
+
+<div class="border border-blue-300 shadow rounded-md p-4 max-w-[274px] w-full mx-auto">
+  <div class="animate-pulse flex space-x-4">
+    <div class="flex-1 space-y-6 py-1">
+      <div class="h-2 bg-slate-700 rounded"></div>
+      <div class="space-y-3">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+          <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+        </div>
+        <div class="h-2 bg-slate-700 rounded"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="border border-blue-300 shadow rounded-md p-4 max-w-[274px] w-full mx-auto">
+  <div class="animate-pulse flex space-x-4">
+    <div class="flex-1 space-y-6 py-1">
+      <div class="h-2 bg-slate-700 rounded"></div>
+      <div class="space-y-3">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+          <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+        </div>
+        <div class="h-2 bg-slate-700 rounded"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="border border-blue-300 shadow rounded-md p-4 max-w-[274px] w-full mx-auto">
+  <div class="animate-pulse flex space-x-4">
+    <div class="flex-1 space-y-6 py-1">
+      <div class="h-2 bg-slate-700 rounded"></div>
+      <div class="space-y-3">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+          <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+        </div>
+        <div class="h-2 bg-slate-700 rounded"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="border border-blue-300 shadow rounded-md p-4 max-w-[274px] w-full mx-auto">
+  <div class="animate-pulse flex space-x-4">
+    <div class="flex-1 space-y-6 py-1">
+      <div class="h-2 bg-slate-700 rounded"></div>
+      <div class="space-y-3">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+          <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+        </div>
+        <div class="h-2 bg-slate-700 rounded"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+          </div>
+  )
+}
